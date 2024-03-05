d3a21ebb7c7536bac4e0f25f2aeca8796500eab2
denfazylov@mailru <denfazylov@mailru>
2024-03-05 19:25:45 +0500

    Прописал функцию создания нового массива по условию(длина эдемента массива не более 3)

--- Program.cs
+++ Program.cs
@@ -9,3 +9,23 @@
 [“Russia”, “Denmark”, “Kazan”] → []
 */
 
+string [] Array(string [] Array) //Объявление функции заполнения нового массива
+{
+  int n = 0;
+  for (int i = 0;i<Array.Length;i++)
+  {
+    if(Array[i].Length <=3) //Подсчет количества элементов, удовлетворяющих условию (не более 3 символов)
+    n++;
+  }
+  string [] newArray = new string [n]; // Создание массива размером равным количеству удовлетворяющих условию
+  int j = 0;
+  for (int i = 0;i<Array.Length;i++)
+  {
+    if(Array[i].Length <=3)
+    {
+        newArray[j] = Array[i]; //Заполнение нового массива
+        j++;
+    }
+  }
+  return newArray;
+}
